7536848f9b58bb9d4175f0d662b25a3d507388ea
leolopezv <leonardo.lopez@uees.edu.ec>
2024-07-26 14:29:13 -0500

    get, add, delete

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fotosRouter = require('./routes/fotos');  // Agregue la referencia al manejador de rutas ’./routes/fotos’
+var restRouter = require('./rest/fotos');  // Agregue la referencia al manejador de rutas ’./rest/fotos’
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/fotos', fotosRouter);  // Agregue la ruta ‘/fotos’
+app.use('/rest/fotos', restRouter);  // Agregue la ruta ‘/rest/fotos’
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
